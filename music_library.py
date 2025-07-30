6c609903cbcfcef7f5ace221221c4bddfb608837
Sulav Shrestha <sthasulav8@gmail.com>
2025-07-30 15:20:00 +0545

    Initial commit

---
+++ music_library.py
@@ -0,0 +1,9 @@
+music = {
+    "six":"https://youtu.be/Fj6cr3FO2JI?si=7Bnd0UZAlRe_SK4H",
+    "all" :"https://youtu.be/JQbjS0_ZfJ0?si=k747QdigDcjUV0JO",
+    "skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI&list=RDDeumyOzKqgI&start_radio=1",
+    "somebody":"https://youtu.be/8UVNT4wvIGY?si=OERqV08S4mKvatwk",
+    "reminder" : "https://youtu.be/JZjAg6fK-BQ",
+    "timeless" : "https://youtu.be/5EpyN_6dqyk",
+    "ruku" :"https://youtu.be/p7wDS1VV3n4"
+}
