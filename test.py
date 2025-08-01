075d10a42871a780579b8c1bef96cc194fb5c76b
Sulav Shrestha <sthasulav8@gmail.com>
2025-08-01 18:46:46 +0545

    steps to follow after renaming file

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from gtts import gTTS
 tts = gTTS('hello')
 tts.save('hello.mp3')
+
+#Rename-Item "J.A.R.V.I.S(AI).py" "J.A.R.V.I.S.py"
+# git add -A
+# git commit -m "Renamed J.A.R.V.I.S(AI).py to J.A.R.V.I.S.py"
+# git push origin main
